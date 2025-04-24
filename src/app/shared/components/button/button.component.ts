4840652a9adc8e5811977bdc8489e120c3a9ef31
Maksym Havryliuk1 <maksym_havryliuk1@epam.com>
2025-04-24 17:42:07 +0300

    customize header button and info components

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Component, Input } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent {
+  @Input() text?: string;
+  @Input() iconName?: string;
+
+  icon?: IconDefinition;
+  library: any;
+
   constructor(library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
 
-  // Use the names for the inputs `buttonText` and `iconName`.
+  ngOnChanges(): void {
+    if (this.iconName) {
+      this.icon = this.library.getIconDefinition(this.iconName);
+    }
+  }
 }
